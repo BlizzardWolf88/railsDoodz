12628466e7239d76c6a75de6e1f8a27b00002316
Bryden Dana <lildukedana@gmail.com>
2023-04-10 09:08:24 -0400

    new ENV for email key

--- config/environment.rb
+++ config/environment.rb
@@ -5,24 +5,9 @@ require_relative "application"
 Rails.application.initialize!
 
 
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.smtp_settings={
-#     address:            'smpt.gmail.com',
-#     port:                465,
-#     domain:              'gmail.com',
-#     user_name:           ENV['GMAIL_USERNAME'],
-#     password:            ENV['GMAIL_PASSWORD'],
-#     authentication:       'plain',
-#     :ssl => true,
-#     :tsl => true,
-#     enable_starttls_auto:true
-
-# }
-
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => 'SG.ZMOqQQ6xSxqeE-PQWTYeeQ.nGeml-eGqM1rNBZQpW7-hImChoFkxMsQSuhTTDN1OwA', # This is the secret sendgrid API key which was issued during API key creation
+    :password => ENV['Email_Key'], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'lildukedana@gmail.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
