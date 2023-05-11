9b7b396cefdef720853cd25c714cf95c5fb57428
Bryden Dana <lildukedana@gmail.com>
2023-05-10 20:51:21 -0400

    We can save wind direc

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -8,81 +8,92 @@ export default class extends Controller {
 
   connect() {
     if (typeof (google) != "undefined"){
-      this.initializeMap();      
+      this.initializeMap();
     }
   }
 
-  
-  initializeMap(evt) {
-    //Don't Invoke these functions for now 
 
-    let northLogo = document.getElementById('northLogo').src = "/assets/wind/North.png";
-    let northWestLogo = document.getElementById('northWestLogo').src = "/assets/wind/NorthWest.png";
-    let northEastLogo = document.getElementById('northEastLogo').src = "/assets/wind/NorthEast.png";
-    let westLogo = document.getElementById('westLogo').src = "/assets/wind/West.png";
-    let eastLogo = document.getElementById('eastLogo').src = "/assets/wind/East.png";
-    let southWestlogo = document.getElementById('southWestLogo').src = "/assets/wind/SouthWest.png";
-    let southEeastlogo = document.getElementById('southEastLogo').src = "/assets/wind/SouthEast.png";
-    let southLogo = document.getElementById('southLogo').src = "/assets/wind/South.png";
+  initializeMap(evt) {
+    //Don't Invoke these functions for now
+
+    const northLogo = document.getElementById('northLogo').src = "/assets/wind/North.png";
+    const northWestLogo = document.getElementById('northWestLogo').src = "/assets/wind/NorthWest.png";
+     const northEastLogo = document.getElementById('northEastLogo').src = "/assets/wind/NorthEast.png";
+     const westLogo = document.getElementById('westLogo').src = "/assets/wind/West.png";
+     const eastLogo = document.getElementById('eastLogo').src = "/assets/wind/East.png";
+     const southWestlogo = document.getElementById('southWestLogo').src = "/assets/wind/SouthWest.png";
+     const southEeastlogo = document.getElementById('southEastLogo').src = "/assets/wind/SouthEast.png";
+     const southLogo = document.getElementById('southLogo').src = "/assets/wind/South.png";
+
+    this.NorthWind = ""
+    this.NorthEastWind = ""
+    this.EastWind = ""
+    this.SouthEastWind = ""
+    this.SouthWind = ""
+    this.SouthWestWind = ""
+    this.WestWind = ""
+    this.NorthWestWind = ""
 
      //Lat and Lon for starting position on map
       const lat = parseFloat("43.639")
       const lon = parseFloat("-71.981" )
       const coordinates = {lat: lat,lng: lon}
       this.map = new google.maps.Map(this.mapTarget,{center:coordinates,zoom:15});
-  
+
 
      this.content = formContent //HTML for the info Window marker form
-        
+
      this.CreateIcons();
-  
+
      this.map.addListener("click", (e) => {
 
         console.log("Map Listener")
-        this.placeMarkerAndPanTo(e.latLng, this.map);   
-        
-      });     
-   
+        this.placeMarkerAndPanTo(e.latLng, this.map);
+
+      });
+
   }
 
 
-     placeMarkerAndPanTo(latLng, map) {   
-     
+     placeMarkerAndPanTo(latLng, map) {
+
+
+
      //Create Marker on the click
      if(this.marker != undefined){
          this.marker.setPosition(latLng);
          this.infowindow.close();
-     } 
-     else{   
+     }
+     else{
        this.marker = new google.maps.Marker({
           position: latLng,
           map: map,
           icon: this.icon1,
         });
         map.panTo(latLng);
-             
-    }   
-        
+
+    }
+
+   
       const cords = JSON.stringify(latLng.toJSON(), null, 2)
       const parsee = JSON.parse(cords);
 
       //send coords to testfields
       this.latTarget.value = parsee.lat
-      this.lonTarget.value = parsee.lng 
+      this.lonTarget.value = parsee.lng
 
 
       this.infowindow = new google.maps.InfoWindow();
-      this.infowindow.setContent(this.content)
+
+      this.infowindow.setContent(this.content)   
       this.infowindow.open(map,this.marker);
-      
-     
-          
-  }   
+
+  }
 
 
   keydown(event){
 
-    if (event.key == "Enter"){      
+    if (event.key == "Enter"){
         this.goToSpot();
     }
   }
@@ -99,97 +110,95 @@ export default class extends Controller {
 
   }
 
-    //INSTANIATE ALL THIS._WIND TO with let and set to "" 
+    //INSTANIATE ALL THIS._WIND TO with let and set to ""
     sendNorth(){
-      if(this.NorthWind == undefined || this.NorthWind == "" ){
+      if(this.NorthWind == "" ){
         this.NorthWind = this.northTarget.dataset.north
         northLogo.src = "/assets/wind/NorthAfter.png"
       }
       else{
         this.NorthWind = ""
-        northLogo.src = "/assets/wind/North.png"  
-      }       
+        northLogo.src = "/assets/wind/North.png"
+      }
     }
 
-     
-   
+
     sendNorthWest(){
-      if(this.NorthWestWind == undefined || this.NorthWestWind == ""){
+      if(this.NorthWestWind == ""){
         this.NorthWestWind = this.northwestTarget.dataset.northwest
         northWestLogo.src = "/assets/wind/NorthWestAfter.png"
       }
       else{
         this.NorthWestWind = ""
-        northWestLogo.src = "/assets/wind/NorthWest.png"  
-      }       
+        northWestLogo.src = "/assets/wind/NorthWest.png"
+      }
     }
 
     sendNorthEast(){
-      if(this.NorthEastWind == undefined || this.NorthEastWind == ""){
+      if(this.NorthEastWind == ""){
         this.NorthEastWind = this.northeastTarget.dataset.northeast
         northEastLogo.src = "/assets/wind/NorthEastAfter.png"
       }
       else{
-        this.NorthEastWind = " "
-        northEastLogo.src = "/assets/wind/NorthEast.png"  
-      }       
+        this.NorthEastWind = ""
+        northEastLogo.src = "/assets/wind/NorthEast.png"
+      }
     }
 
     sendWest(){
-      if(this.WestWind == undefined){
+      if(this.WestWind == ""){
         this.WestWind = this.westTarget.dataset.west
         westLogo.src = "/assets/wind/WestAfter.png"
       }
       else{
-        this.WestWind = null  
+        this.WestWind = ""
         westLogo.src = "/assets/wind/West.png"
-      }       
+      }
     }
 
-    sendEast(){      
-      if(this.EastWind == undefined){
+    sendEast(){
+      if(this.EastWind == ""){
         this.EastWind  = this.eastTarget.dataset.east
         eastLogo.src = "/assets/wind/EastAfter.png"
       }
       else{
-        this.EastWind = null  
+        this.EastWind = ""
         eastLogo.src = "/assets/wind/East.png"
-      }    
+      }
     }
 
-    sendSouthWest(){      
-      if(this.SouthWestWind == undefined){
+    sendSouthWest(){
+      if(this.SouthWestWind == ""){
         this.SouthWestWind  = this.southwestTarget.dataset.southwest
         southWestLogo.src = "/assets/wind/SouthWestAfter.png"
       }
       else{
-        this.SouthWestWind = null  
+        this.SouthWestWind = ""
         southWestLogo.src = "/assets/wind/SouthWest.png"
-      }    
+      }
     }
 
-    sendSouthEast(){      
-      if(this.SouthEastWind == undefined){
+    sendSouthEast(){
+      if(this.SouthEastWind == ""){
         this.SouthEastWind  = this.southeastTarget.dataset.southeast
         southEastLogo.src = "/assets/wind/SouthEastAfter.png"
       }
       else{
-        this.SouthEastWind = null  
+        this.SouthEastWind = ""
         southEastLogo.src = "/assets/wind/SouthEast.png"
-      }    
+      }
     }
 
     sendSouth(){
-      if(this.SouthWind == undefined){
+      if(this.SouthWind == ""){
         this.SouthWind  = this.southTarget.dataset.south
         southLogo.src = "/assets/wind/SouthAfter.png"
       }
       else{
-        this.SouthWind = null  
+        this.SouthWind = ""
         southLogo.src = "/assets/wind/South.png"
-      }  
-    }    
-
+      }
+    }
 
 
     CreateIcons(){
@@ -213,8 +222,9 @@ export default class extends Controller {
 
   saveSpot(event) {
 
-    //var sendWind = this.NorthWind +"," + this.NorthEastWind + "," + this.NorthWestWind
-    var sendWind = "NW"
+    var sendWind = this.NorthWind +"," + this.NorthEastWind + "," + this.EastWind + "," +
+    this.SouthEastWind + "," + this.SouthWind +"," + this.SouthWestWind + "," + this.WestWind +"," + this.NorthWestWind
+
     const id = event.target.dataset.id
     const csrfToken = document.querySelector("[name='csrf-token']").content
 
@@ -227,9 +237,9 @@ export default class extends Controller {
             'Content-Type': 'application/json',
             'X-CSRF-Token': csrfToken
         },
-        body: JSON.stringify({ 
+        body: JSON.stringify({
           id:this.id,
-          name: this.nameTarget.value, 
+          name: this.nameTarget.value,
           latitude: this.latTarget.value,
           longitude:this.lonTarget.value,
           notes: this.notesTarget.value,
@@ -241,17 +251,36 @@ export default class extends Controller {
        //.then(data => {alert(data.message)})
 
        //CALL A RESET FUNCTION
-        this.nameTarget.value = ""
-        this.dateTarget.value = ""
-        this.notesTarget.value = ""
-        this.numsitsTarget.value = ""
+        this.clearMarker();
 
         this.infowindow.close();
 
   }
- 
 
-    
+  clearMarker(){
+
+    //DON'T FORGET TO ADD THE TYPE OF ICON FOR
+    //  northLogo.src = "/assets/wind/North.png";
+    //  northWestLogo.src = "/assets/wind/NorthWest.png";
+    //  northEastLogo.src = "/assets/wind/NorthEast.png";
+    //  westLogo.src = "/assets/wind/West.png";
+    //  eastLogo.src = "/assets/wind/East.png";
+    //  southWestlogo.src = "/assets/wind/SouthWest.png";
+    //  southEeastlogo.src = "/assets/wind/SouthEast.png";
+    //  southLogo.src = "/assets/wind/South.png";
+
+
+    this.nameTarget.value = ""
+    this.dateTarget.value = ""
+    this.notesTarget.value = ""
+    this.numsitsTarget.value = ""
+
+
+
+  }
+
+
+
   }
 
 
